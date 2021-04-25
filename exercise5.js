ca3b32c0987e6427e101fd2af793396b57bceaf0
Ashutosh-Maurya-87 <ashumaurya486@gmail.com>
2021-04-25 09:04:00 +0530

    This_is_exercise

---
+++ exercise5.js
@@ -0,0 +1,188 @@
+console.log("welcome to the fetch api");
+let myObj = `{
+    "word": "example",
+    "results": [
+      {
+        "definition": "a representative form or pattern",
+        "partOfSpeech": "noun",
+        "synonyms": [
+          "model"
+        ],
+        "typeOf": [
+          "representation",
+          "internal representation",
+          "mental representation"
+        ],
+        "hasTypes": [
+          "prefiguration",
+          "archetype",
+          "epitome",
+          "guide",
+          "holotype",
+          "image",
+          "loadstar",
+          "lodestar",
+          "microcosm",
+          "original",
+          "paradigm",
+          "pilot",
+          "prototype",
+          "template",
+          "templet",
+          "type specimen"
+        ],
+        "derivation": [
+          "exemplify"
+        ],
+        "examples": [
+          "I profited from his example"
+        ]
+      },
+      {
+        "definition": "something to be imitated",
+        "partOfSpeech": "noun",
+        "synonyms": [
+          "exemplar",
+          "good example",
+          "model"
+        ],
+        "typeOf": [
+          "ideal"
+        ],
+        "hasTypes": [
+          "pacemaker",
+          "pattern",
+          "beauty",
+          "prodigy",
+          "beaut",
+          "pacesetter"
+        ],
+        "derivation": [
+          "exemplify",
+          "exemplary"
+        ]
+      },
+      {
+        "definition": "an occurrence of something",
+        "partOfSpeech": "noun",
+        "synonyms": [
+          "case",
+          "instance"
+        ],
+        "typeOf": [
+          "happening",
+          "natural event",
+          "occurrence",
+          "occurrent"
+        ],
+        "hasTypes": [
+          "clip",
+          "mortification",
+          "piece",
+          "time",
+          "humiliation",
+          "bit"
+        ],
+        "derivation": [
+          "exemplify"
+        ],
+        "examples": [
+          "but there is always the famous example of the Smiths"
+        ]
+      },
+      {
+        "definition": "an item of information that is typical of a class or group",
+        "partOfSpeech": "noun",
+        "synonyms": [
+          "illustration",
+          "instance",
+          "representative"
+        ],
+        "typeOf": [
+          "information"
+        ],
+        "hasTypes": [
+          "excuse",
+          "apology",
+          "specimen",
+          "case in point",
+          "sample",
+          "exception",
+          "quintessence",
+          "precedent"
+        ],
+        "derivation": [
+          "exemplify",
+          "exemplary"
+        ],
+        "examples": [
+          "this patient provides a typical example of the syndrome",
+          "there is an example on page 10"
+        ]
+      },
+      {
+        "definition": "punishment intended as a warning to others",
+        "partOfSpeech": "noun",
+        "synonyms": [
+          "deterrent example",
+          "lesson",
+          "object lesson"
+        ],
+        "typeOf": [
+          "monition",
+          "admonition",
+          "word of advice",
+          "warning"
+        ],
+        "derivation": [
+          "exemplary"
+        ],
+        "examples": [
+          "they decided to make an example of him"
+        ]
+      },
+      {
+        "definition": "a task performed or problem solved in order to develop skill or understanding",
+        "partOfSpeech": "noun",
+        "synonyms": [
+          "exercise"
+        ],
+        "typeOf": [
+          "lesson"
+        ],
+        "examples": [
+          "you must work the examples at the end of each chapter in the textbook"
+        ]
+      }
+    ],
+    "syllables": {
+      "count": 3,
+      "list": [
+        "ex",
+        "am",
+        "ple"
+      ]
+    },
+    "pronunciation": {
+      "all": "ɪɡ'zæmpəl"
+    },
+    "frequency": 4.67
+  }`;
+const obj = JSON.parse(myObj);
+console.log(obj);
+console.log(obj["results"]);
+console.log(obj.syllables);
+let fetchApi = document.getElementById("btn");
+btn.addEventListener("click", function (e) {
+  console.log("anyone click the fetch btn");
+  populate();
+});
+function populate() {
+  let result = obj["results"];
+  let html = "";
+  result.forEach((element) => {
+    html += `<li>one of the difinitions of example are ${element.definition}</li>`;
+  });
+  let defs = document.getElementById("defs");
+  defs.innerHTML = html;
+}
